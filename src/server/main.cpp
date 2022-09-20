4c4efcc022784e46e92dbca703ed9ea5ec293c3f
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-09-20 14:07:06 +0200

    [ADD] Cmake architecture

---
+++ src/server/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** Main.cpp
+** File description:
+** main file of server
+*/
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello Server!" << std::endl;
+    return 0;
+}
