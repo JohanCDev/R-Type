41504d5aa421663a77f7f09d0c298fd1d3317613
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-09-20 14:39:11 +0200

    [ADD] Documentation generation

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,16 +1,30 @@
+/**
+ * @file main.cpp
+ * @author Johan Chrillesen (johan.chrillesen@epitech.eu)
+ * @brief Main file of the client
+ * @version 0.1
+ * @date 2022-09-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include <SFML/Window.hpp>
 
+/**
+ * @brief main function of the client
+ *
+ * @return 0 if success or 84 if error
+ */
 int main()
 {
     sf::Window window(sf::VideoMode(800, 600), "My window");
 
     // run the program as long as the window is open
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,12 +1,21 @@
-/*
-** EPITECH PROJECT, 2022
-** Main.cpp
-** File description:
-** main file of server
-*/
+/**
+ * @file main.cpp
+ * @author Johan Chrillesen (johan.chrillesen@epitech.eu)
+ * @brief Main file of the server
+ * @version 0.1
+ * @date 2022-09-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
 #include <iostream>
 
+/**
+ * @brief main function of the server
+ *
+ * @return 0 if success or 84 if error
+ */
 int main(void)
 {
     std::cout << "Hello Server!" << std::endl;
