2b070f0de5822d3e538ecbf5cdbab67c41f75bcb
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-09-28 17:09:15 +0200

    [ADD] Dependencies are downloaded on linux

---
+++ install.sh
@@ -0,0 +1,12 @@
+#/usr/bin/bash
+
+if test -d build
+then
+    rm -rf ./build;
+fi
+
+mkdir build && cd build && conan install .. --build=missing \
+-c tools.system.package_manager:mode=install \
+-s compiler.version=13.1 \
+-c tools.system.package_manager:sudo=True && \
+cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build  . -j 3
