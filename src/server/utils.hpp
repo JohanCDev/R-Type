4bfd8f995030f57c902682db2d1391c6648110e2
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-09-28 15:37:11 +0200

    [ADD] Header file for server

---
+++ src/server/utils.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+std::string send_packet(size_t type, std::string content);
+void stream2hex(const std::string str, std::string &hexstr, bool capital = false);
+size_t encode_to_byte(void *output, uint32_t input);
+size_t push_back_byte(char buffer[], uint32_t value);
