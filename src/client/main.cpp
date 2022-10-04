35a6c0f5cf1e7a9becd7fcedfa495b8fffc3b594
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-10-04 19:02:59 +0200

    [REF] Main file to check if linter actually work

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -21,10 +21,12 @@ int main()
     sf::Window window(sf::VideoMode(800, 600), "My window");
 
     // run the program as long as the window is open
-    while (window.isOpen()) {
+    while (window.isOpen())
+    {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event))
+        {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
