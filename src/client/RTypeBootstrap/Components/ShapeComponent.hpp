531422229a5110ff575e17d8e90705e721b65675
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-10-04 16:56:02 +0200

    [REF] Clean file

--- src/client/RTypeBootstrap/Components/ShapeComponent.hpp
+++ src/client/RTypeBootstrap/Components/ShapeComponent.hpp
@@ -1,52 +1,42 @@
 /**
  * @file ShapeComponent.hpp
  * @author Cédric Corge (cedric.corge@epitech.eu)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-10-04
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #pragma once
 
 #include <string>
 
-enum shape_type {
-    RECTANGLE,
-    TRIANGLE,
-    CIRCLE,
-    TEXT
-};
+enum shape_type { RECTANGLE, TRIANGLE, CIRCLE, TEXT };
 
 struct ShapeComponent {
-
     float x_scale;
-	float y_scale;
+    float y_scale;
     shape_type type;
     int radius;
-    int side;
     int length;
     int width;
-    std::string font;
     std::string text;
+    std::string font;
     int font_size;
 
-    ShapeComponent(enum shape_type shape, int length) {
-        if (shape == shape_type::CIRCLE) {
-            type = shape;
-            radius = length;
-        } else {
-            type = shape;
-            side = length;
-        }
+    ShapeComponent(enum shape_type type_shape, int set_radius) : type(type_shape), radius(set_radius)
+    {
     }
 
     ShapeComponent(enum shape_type type_shape, int length_value, int width_value)
-    : type(type_shape), length(length_value), width(width_value) {}
+        : type(type_shape), length(length_value), width(width_value)
+    {
+    }
 
     ShapeComponent(enum shape_type type_shape, std::string text_string, std::string font_path, int char_size)
-    : type(type_shape), text(text_string), font(font_path), font_size(char_size) {}
-
+        : type(type_shape), text(text_string), font(font_path), font_size(char_size)
+    {
+    }
 };
