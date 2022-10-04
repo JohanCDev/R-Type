caf0c6ee729839d325a3a832cb0e314c27bf4dd1
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-10-04 16:11:15 +0200

    [ADD] Handle shape drawing

--- src/client/RTypeBootstrap/Systems/DrawableSystem.cpp
+++ src/client/RTypeBootstrap/Systems/DrawableSystem.cpp
@@ -1,18 +1,21 @@
 #include "../registry.h"
 #include "../Components/DrawableComponent.h"
+#include "../Components/ShapeComponent.hpp"
 #include "../Components/PositionComponent.h"
 #include "../AssetsManager.hpp"
+#include <iostream>
 
 int drawable_system(registry &r, sf::RenderWindow &window, AssetsManager manager)
 {
     sparse_array<DrawableComponent> &drawable = r.get_components<DrawableComponent>();
+    sparse_array<ShapeComponent> &shape = r.get_components<ShapeComponent>();
     sparse_array<PositionComponent> &position = r.get_components<PositionComponent>();
     std::size_t index = 0;
     sf::Sprite sprite;
     sf::Texture texture;
 
 	for (auto &i : drawable) {
-        if (i && i.has_value()) {
+        if (i && i.has_value() && !shape[index]) {
             sf::Vector2f vec(position[index]->x, position[index]->y);
             texture = manager.get_texture(i->path);
             sprite.setTexture(texture);
@@ -22,5 +25,18 @@ int drawable_system(registry &r, sf::RenderWindow &window, AssetsManager manager
 		}
         index++;
 	}
+    index = 0;
+    for (auto &entity : shape) {
+        if (entity && entity.has_value()) {
+            if (entity->type == shape_type::RECTANGLE) {
+                sf::RectangleShape rect(sf::Vector2f(entity->length, entity->width));
+                rect.setPosition(position[index]->x, position[index]->y);
+                rect.setFillColor(sf::Color::Red);
+                window.draw(rect);
+                break;
+            }
+        }
+        index++;
+    }
     return (0);
 }
