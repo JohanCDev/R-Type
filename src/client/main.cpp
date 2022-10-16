9089a783aa97cd2e2b5eded5e3d6805156a2a2b8
Cedric <cedric.corge@epitech.eu>
2022-10-16 14:33:06 +0200

    [UPD] move ECS to src/ folder

---
+++ src/client/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file main.cpp
+ * @author Cédric Corge (cedric.corge@epitech.eu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <SFML/Graphics.hpp>
+
+int main(void) {
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+    return (0);
+}
