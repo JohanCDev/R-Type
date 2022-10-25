c17a40952446a3ace023602758e25b266ce6cd2f
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-10-26 00:00:21 +0200

    [ADD] Create enemy in the server's world and send packet

--- src/server/handle_packets.cpp
+++ src/server/handle_packets.cpp
@@ -26,10 +26,10 @@ void player_joined(World &world, ClientMessage msg, NetworkServer &server)
 {
     size_t id = 0;
     id = world.create_player("", Vector4i(375, 6, 21, 24), Vector2f(DEFAULT_PLAYER_SCALE, DEFAULT_PLAYER_SCALE),
-        Vector2i(DEFAULT_PLAYER_POS_X, DEFAULT_PLAYER_POS_Y), DEFAULT_PLAYER_HP, Vector2i(0, 0), 0.2, DEFAULT_KEY_TOP, DEFAULT_KEY_BOT,
-        DEFAULT_KEY_RGT, DEFAULT_KEY_LFT, DEFAULT_KEY_SHOOT);
+        Vector2i{DEFAULT_PLAYER_POS_X, DEFAULT_PLAYER_POS_Y}, DEFAULT_PLAYER_HP, Vector2i{0, 0}, 0.2, DEFAULT_KEY_TOP,
+        DEFAULT_KEY_BOT, DEFAULT_KEY_RGT, DEFAULT_KEY_LFT, DEFAULT_KEY_SHOOT);
     world.getRegistry().add_component<ClientIDComponent>(
-        world.getRegistry().entity_from_index(id), ClientIDComponent(msg.second));
+        world.getRegistry().entity_from_index(id), ClientIDComponent{msg.second});
     std::cout << "Player[" << msg.second << "]: joined the game" << std::endl;
 }
 
@@ -41,7 +41,7 @@ void player_left(World &world, ClientMessage msg, NetworkServer &server)
 
 void player_moved(World &world, ClientMessage msg, NetworkServer &server)
 {
-    Vector2f move(0, 0);
+    Vector2i move{0, 0};
     registry &r = world.getRegistry();
     sparse_array<VelocityComponent> &velocity = r.get_components<VelocityComponent>();
     sparse_array<ClientIDComponent> &clients = r.get_components<ClientIDComponent>();
@@ -53,7 +53,8 @@ void player_moved(World &world, ClientMessage msg, NetworkServer &server)
             if (i.value().id == msg.second) {
                 velocity[index]->speed.x = move.x * DEFAULT_PLAYER_SPD;
                 velocity[index]->speed.y = move.y * DEFAULT_PLAYER_SPD;
-                std::cout << "Player[" << msg.second << "]: Velocity{" << move.x * DEFAULT_PLAYER_SPD << ", " << move.y * DEFAULT_PLAYER_SPD << "}" << std::endl;
+                std::cout << "Player[" << msg.second << "]: Velocity{" << move.x * DEFAULT_PLAYER_SPD << ", "
+                          << move.y * DEFAULT_PLAYER_SPD << "}" << std::endl;
             }
         }
         index++;
@@ -63,18 +64,24 @@ void player_moved(World &world, ClientMessage msg, NetworkServer &server)
 void player_shot(World &world, ClientMessage msg, NetworkServer &server)
 {
     std::cout << "Player Shot" << std::endl;
-    //world.create_laser(1, 1, DEFAULT_LASER_X_SPD, DEFAULT_LASER_Y_SPD, 0.2, 0);
+    // world.create_laser(1, 1, DEFAULT_LASER_X_SPD, DEFAULT_LASER_Y_SPD, 0.2, 0);
 }
 
 void create_enemy(World &world, NetworkServer &server)
 {
-    (void)server;
+    size_t id = 0;
     int random_y = rand() % 400 + 51;
     Message<GameMessage> msg;
+
+    id = world.create_enemy("r-typesheet39.gif", Vector4i(34, 2, 64, 64),
+        Vector2f(DEFAULT_ENEMY_SCALE, DEFAULT_ENEMY_SCALE), Vector2i{0, random_y},
+        Vector2i{DEFAULT_ENEMY_SPD, DEFAULT_ENEMY_SPD}, 0.2, 0);
+    world.getRegistry().add_component<EntityIDComponent>(
+        world.getRegistry().entity_from_index(id), EntityIDComponent{id});
+    std::cout << "Enemy[" << id << "]: created at Position{840, " << random_y << "}" << std::endl;
     msg.header.id = GameMessage::S2C_ENTITY_NEW;
-    msg << EntityCode::ENEMY;
+    msg << GameObject::ENEMY;
+    msg << id;
     msg << sf::Vector2f(0, random_y);
-    world.create_enemy("r-typesheet39.gif", Vector4i(34, 2, 64, 64), Vector2f(DEFAULT_ENEMY_SCALE, DEFAULT_ENEMY_SCALE), Vector2i(0,
-        random_y), Vector2i(DEFAULT_ENEMY_SPD, DEFAULT_ENEMY_SPD), 0.2, 0);
-    // server.SendToAll(msg);
+    server.SendToAll(msg);
 }
