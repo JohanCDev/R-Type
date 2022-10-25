a13779920a810897169dd2b6f41cd41de3270135
Louis <louis.maestre@epitech.eu>
2022-10-25 15:49:53 +0200

    [UPD] add dead entity

--- src/Common/Message/MessageType.hpp
+++ src/Common/Message/MessageType.hpp
@@ -11,6 +11,6 @@
 
 #pragma once
 
-enum class GameMessage : uint32_t { C2S_JOIN, C2S_LEAVE, S2C_ENTITY_NEW };
+enum class GameMessage : uint32_t { C2S_JOIN, C2S_LEAVE, S2C_ENTITY_NEW, S2C_ENTITY_DEAD };
 
 enum class GameObject : uint32_t { LASER, PLAYER, ENEMY };
