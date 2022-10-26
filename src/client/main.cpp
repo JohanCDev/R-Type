00b59f315f3a4d13a18c2cb3448ea1848b210ebe
Cedric <cedric.corge@epitech.eu>
2022-10-26 13:48:11 +0200

    [REF] Handle movement

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -20,70 +20,7 @@
 
 #include "../ECS/World.hpp"
 
-void cancel_movement(World &world, NetworkClient &client, sf::Event event)
-{
-    Message<GameMessage> moveMsg;
-    moveMsg.header.id = GameMessage::C2S_MOVEMENT;
-    Vector2f current_direction = world.getDirection();
-
-    if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Z) {
-        moveMsg << Vector2f{current_direction.x, 0};
-        client.send(moveMsg);
-        world.setDirection({current_direction.x, 0});
-    }
-    if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::S) {
-        moveMsg << Vector2f{current_direction.x, 0};
-        client.send(moveMsg);
-        world.setDirection({current_direction.x, 0});
-    }
-    if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Q) {
-        moveMsg << Vector2f{0, current_direction.y};
-        client.send(moveMsg);
-        world.setDirection({0, current_direction.y});
-    }
-    if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::D) {
-        moveMsg << Vector2f{0, current_direction.y};
-        client.send(moveMsg);
-        world.setDirection({0, current_direction.y});
-    }
-}
-
-void handle_movement(World &world, NetworkClient &client, sf::Event event)
-{
-    Message<GameMessage> moveMsg;
-    moveMsg.header.id = GameMessage::C2S_MOVEMENT;
-    Vector2f current_direction = world.getDirection();
-
-    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Z) {
-        if (current_direction.y != 1) {
-            moveMsg << Vector2f{current_direction.x, 1};
-            client.send(moveMsg);
-            world.setDirection({current_direction.x, 1});
-        }
-    }
-    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S) {
-        if (current_direction.y != -1) {
-            moveMsg << Vector2f{current_direction.x, -1};
-            client.send(moveMsg);
-            world.setDirection({current_direction.x, -1});
-        }
-    }
-    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Q) {
-        if (current_direction.x != -1) {
-            moveMsg << Vector2f{-1, current_direction.y};
-            client.send(moveMsg);
-            world.setDirection({-1, current_direction.y});
-        }
-    }
-    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::D) {
-        if (current_direction.x != 1) {
-            moveMsg << Vector2f{1, current_direction.y};
-            client.send(moveMsg);
-            world.setDirection({1, current_direction.y});
-        }
-    }
-    cancel_movement(world, client, event);
-}
+void handle_movement(World &world, NetworkClient &client, sf::Event event);
 
 int main(void)
 {
