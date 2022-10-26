9a0c5e9537899b86f20bd2728fe0388a20410327
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-10-27 00:23:04 +0200

    [FIX] Send only 1 packet when keeping UP or DOWN

--- src/client/handle_movement.cpp
+++ src/client/handle_movement.cpp
@@ -47,14 +47,14 @@ void handle_movement(World &world, NetworkClient &client, sf::Event event)
     Vector2i current_direction = world.getDirection();
 
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Z) {
-        if (current_direction.y != 1) {
+        if (current_direction.y != -1) {
             moveMsg << Vector2i{current_direction.x, -1};
             client.send(moveMsg);
             world.setDirection({current_direction.x, -1});
         }
     }
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S) {
-        if (current_direction.y != -1) {
+        if (current_direction.y != 1) {
             moveMsg << Vector2i{current_direction.x, 1};
             client.send(moveMsg);
             world.setDirection({current_direction.x, 1});
