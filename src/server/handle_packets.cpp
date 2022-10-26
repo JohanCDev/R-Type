509d41e810b0511d340d9a468501dc80d9e96710
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-10-26 14:06:49 +0200

    [ADD] Send packets back when player joined/left

--- src/server/handle_packets.cpp
+++ src/server/handle_packets.cpp
@@ -25,6 +25,8 @@
 void player_joined(World &world, ClientMessage msg, NetworkServer &server)
 {
     size_t entity_id = 0;
+    Message<GameMessage> sending_msg;
+
     entity_id = world.create_player("", Vector4i(375, 6, 21, 24), Vector2f(DEFAULT_PLAYER_SCALE, DEFAULT_PLAYER_SCALE),
         Vector2i{DEFAULT_PLAYER_POS_X, DEFAULT_PLAYER_POS_Y}, DEFAULT_PLAYER_HP, Vector2i{0, 0}, 0.2, DEFAULT_KEY_TOP,
         DEFAULT_KEY_BOT, DEFAULT_KEY_RGT, DEFAULT_KEY_LFT, DEFAULT_KEY_SHOOT);
@@ -33,12 +35,32 @@ void player_joined(World &world, ClientMessage msg, NetworkServer &server)
     world.getRegistry().add_component<EntityIDComponent>(
         world.getRegistry().entity_from_index(entity_id), EntityIDComponent{entity_id});
     std::cout << "Player[" << msg.second << "]: joined the game" << std::endl;
+    sending_msg.header.id = GameMessage::S2C_ENTITY_NEW;
+    sending_msg << GameObject::PLAYER;
+    sending_msg << entity_id;
+    sending_msg << Vector2f{DEFAULT_PLAYER_POS_X, DEFAULT_PLAYER_POS_Y};
+    server.SendToAll(sending_msg);
 }
 
 void player_left(World &world, ClientMessage msg, NetworkServer &server)
 {
+    registry &r = world.getRegistry();
+    sparse_array<ClientIDComponent> &clients = r.get_components<ClientIDComponent>();
+    sparse_array<EntityIDComponent> &entities = r.get_components<EntityIDComponent>();
+    Message<GameMessage> sending_msg;
+    size_t index = 0;
+
     std::cout << "Player[" << msg.second << "]: left the game" << std::endl;
-    (void)world;
+    for (auto &i : clients) {
+        if (i && i.has_value() && i.value().id == msg.second) {
+            r.kill_entity(r.entity_from_index(index));
+            sending_msg.header.id = GameMessage::S2C_ENTITY_DEAD;
+            sending_msg << entities[index]->id;
+            server.SendToAll(sending_msg);
+            break;
+        }
+        index++;
+    }
 }
 
 void player_moved(World &world, ClientMessage msg, NetworkServer &server)
@@ -80,7 +102,8 @@ void player_shot(World &world, ClientMessage msg, NetworkServer &server)
     for (auto &i : clients) {
         if (i && i.has_value()) {
             if (i.value().id == msg.second) {
-                entity_id = world.create_laser(Vector2i{position[index]->pos.x, position[index]->pos.y}, Vector2i{DEFAULT_LASER_SPD, 0}, 0.2, 0);
+                entity_id = world.create_laser(
+                    Vector2i{position[index]->pos.x, position[index]->pos.y}, Vector2i{DEFAULT_LASER_SPD, 0}, 0.2, 0);
                 std::cout << "Player[" << msg.second << "]: shot from Position{" << position[index]->pos.x << ", "
                           << position[index]->pos.y << "}" << std::endl;
                 sending_msg.header.id = GameMessage::S2C_ENTITY_NEW;
