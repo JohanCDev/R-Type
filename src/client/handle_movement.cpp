e303b9c2bfd3c7c4a2b88efb2937dd365168a0d0
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-10-26 23:44:58 +0200

    [FIX] Going down when clicking on S

--- src/client/handle_movement.cpp
+++ src/client/handle_movement.cpp
@@ -48,16 +48,16 @@ void handle_movement(World &world, NetworkClient &client, sf::Event event)
 
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Z) {
         if (current_direction.y != 1) {
-            moveMsg << Vector2i{current_direction.x, 1};
+            moveMsg << Vector2i{current_direction.x, -1};
             client.send(moveMsg);
-            world.setDirection({current_direction.x, 1});
+            world.setDirection({current_direction.x, -1});
         }
     }
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S) {
         if (current_direction.y != -1) {
-            moveMsg << Vector2i{current_direction.x, -1};
+            moveMsg << Vector2i{current_direction.x, 1};
             client.send(moveMsg);
-            world.setDirection({current_direction.x, -1});
+            world.setDirection({current_direction.x, 1});
         }
     }
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Q) {
