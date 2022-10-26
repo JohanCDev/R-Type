e96f2309bb03f2e0c60798dfc000f2c09ca41364
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-10-27 00:24:16 +0200

    [REF] Change sleep to 10ms

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -72,7 +72,7 @@ int main(void)
         }
         world.getWindow().display();
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
     client.send(byeMsg);
     return 0;
