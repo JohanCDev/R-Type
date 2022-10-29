ed5102d42f29304ebcc9a2cb4c4ecb4d6bff97dd
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-10-29 18:08:24 +0200

    [REF] Change game default values

--- src/server/game.hpp
+++ src/server/game.hpp
@@ -18,13 +18,13 @@
 
 #define DEFAULT_PLAYER_POS_X 50
 #define DEFAULT_PLAYER_POS_Y 200
-#define DEFAULT_PLAYER_SPD   8
+#define DEFAULT_PLAYER_SPD   6
 #define DEFAULT_PLAYER_HP    100
 #define DEFAULT_PLAYER_SCALE 2.0
 #define DEFAULT_LASER_SPD    5
 #define DEFAULT_ENEMY_HP     100
 #define DEFAULT_ENEMY_ATK    40
-#define DEFAULT_ENEMY_SPD    6
+#define DEFAULT_ENEMY_SPD    4
 #define DEFAULT_ENEMY_SCALE  1.0
 #define DEFAULT_KEY_TOP      KeyboardInput::Z
 #define DEFAULT_KEY_RGT      KeyboardInput::D
