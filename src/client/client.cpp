0f9d63aff3dc9e786d610e42b371f552d0b51e0c
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-11-04 22:56:41 +0100

    [FIX] Warning of unused parameter

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -10,10 +10,10 @@
  */
 
 #include "client.hpp"
-#include "proto.hpp"
 #include <boost/iostreams/stream.hpp>
 #include <boost/serialization/vector.hpp>
 #include <sstream>
+#include "proto.hpp"
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/iostreams/device/back_inserter.hpp>
@@ -194,7 +194,8 @@ void entity_hit(World &world, Message<GameMessage> msg)
             if (idCompo->id == hit_id.id) {
                 health[index]->hp -= damage;
                 health[index]->max_hp = max_hp;
-                std::cout << "Entity[" << hit_id.id << "]: -" << damage << "HP, now has " << health[index]->hp << "/" << max_hp << "HP" << std::endl;
+                std::cout << "Entity[" << hit_id.id << "]: -" << damage << "HP, now has " << health[index]->hp << "/"
+                          << max_hp << "HP" << std::endl;
                 break;
             }
         }
@@ -213,6 +214,7 @@ void wave_status(World &world, Message<GameMessage> msg)
 {
     size_t nb_wave = 0;
     WaveStatus status;
+    (void)world;
 
     msg >> nb_wave;
     msg >> status;
