0c54b890a6d290ca6dafb7a0fd9330036f7758fe
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-11-08 08:55:32 +0100

    [ADD] AI of ODD fixed (don't go outside anymore)

--- src/server/AISystem.cpp
+++ src/server/AISystem.cpp
@@ -68,23 +68,39 @@ static std::map<size_t, Vector2i> next_dir = {{1, {0, -1}}, {2, {1, 0}}, {3, {0,
 
 void update_enemy_odd(World &world, NetworkServer &server, size_t i)
 {
-    size_t random = rand() % 1000;
-    Vector2i topLeft = {0, 50};
-    Vector2i botRight = {700, 550};
-    Vector2i nextPos = {0, 0};
+    size_t random_spawn = rand() % 1000;
+    size_t random_dir = rand() % 1000;
+    Vector2i top_bot = {50, 550};
+    Vector2i left_right = {0, 700};
+    Vector2i next_pos = {0, 0};
+    Message<GameMessage> sending_msg;
+    auto &positions = world.getRegistry().get_components<PositionComponent>();
+    auto &velocity = world.getRegistry().get_components<VelocityComponent>();
+    auto &entities = world.getRegistry().get_components<EntityIDComponent>();
 
-    if (random < 10) {
-        auto &velocity = world.getRegistry().get_components<VelocityComponent>();
-        auto &entities = world.getRegistry().get_components<EntityIDComponent>();
-        auto &positions = world.getRegistry().get_components<PositionComponent>();
-        Message<GameMessage> sending_msg;
-        random = rand() % 4 + 1;
-
-        if (positions[i]->pos.x < botRight.x) {
-            nextPos.x = positions[i]->pos.x + defaultValues[GameObject::ENEMY_ODD].spd * next_dir[random].x;
-            nextPos.y = positions[i]->pos.y + defaultValues[GameObject::ENEMY_ODD].spd * next_dir[random].y;
-            velocity[i]->speed.x = defaultValues[GameObject::ENEMY_ODD].spd * next_dir[random].x;
-            velocity[i]->speed.y = defaultValues[GameObject::ENEMY_ODD].spd * next_dir[random].y;
+    random_dir = rand() % 4 + 1;
+    if (positions[i]->pos.x < left_right.y) {
+        next_pos.x = positions[i]->pos.x + next_dir[random_dir].x * defaultValues[GameObject::ENEMY_ODD].spd;
+        next_pos.y = positions[i]->pos.y + next_dir[random_dir].y * defaultValues[GameObject::ENEMY_ODD].spd;
+        if (next_pos.y <= top_bot.x || next_pos.y >= top_bot.y || next_pos.x >= left_right.y
+            || next_pos.x <= left_right.x) {
+            while (next_pos.y <= top_bot.x || next_pos.y >= top_bot.y || next_pos.x >= left_right.y
+                || next_pos.x <= left_right.x) {
+                random_dir = rand() % 4 + 1;
+                next_pos.x = positions[i]->pos.x + next_dir[random_dir].x * defaultValues[GameObject::ENEMY_ODD].spd;
+                next_pos.y = positions[i]->pos.y + next_dir[random_dir].y * defaultValues[GameObject::ENEMY_ODD].spd;
+            }
+            velocity[i]->speed.x = defaultValues[GameObject::ENEMY_ODD].spd * next_dir[random_dir].x;
+            velocity[i]->speed.y = defaultValues[GameObject::ENEMY_ODD].spd * next_dir[random_dir].y;
+            sending_msg.header.id = GameMessage::S2C_MOVEMENT;
+            sending_msg << entities[i]->id;
+            sending_msg << velocity[i]->speed;
+            server.SendToAll(sending_msg);
+            return;
+        }
+        if (random_spawn < 10) {
+            velocity[i]->speed.x = defaultValues[GameObject::ENEMY_ODD].spd * next_dir[random_dir].x;
+            velocity[i]->speed.y = defaultValues[GameObject::ENEMY_ODD].spd * next_dir[random_dir].y;
             sending_msg.header.id = GameMessage::S2C_MOVEMENT;
             sending_msg << entities[i]->id;
             sending_msg << velocity[i]->speed;
@@ -93,19 +109,27 @@ void update_enemy_odd(World &world, NetworkServer &server, size_t i)
     }
 }
 
+/**
+ * @brief Call a function by a GameObject type
+ */
 static std::map<GameObject, std::function<void(World &, NetworkServer &, size_t)>> mapFunc = {
     {GameObject::ENEMY_FOCUS, update_enemy_focus}, {GameObject::ENEMY_KAMIKAZE, update_enemy_kamikaze},
     {GameObject::ENEMY_ODD, update_enemy_odd}, {GameObject::ENEMY_SNIPER, update_enemy_sniper}};
 
+/**
+ * @brief
+ *
+ * @param world The world
+ * @param server The server
+ * @return int
+ */
 int ia_system(World &world, NetworkServer &server)
 {
     auto &teams = world.getRegistry().get_components<GameTeamComponent>();
     auto &objects = world.getRegistry().get_components<GameObjectComponent>();
 
-    for (size_t i = 0; i < teams.size(); ++i) {
-        if (teams[i] && teams[i]->team == GameTeam::ENEMY) {
+    for (size_t i = 0; i < teams.size(); ++i)
+        if (teams[i] && teams[i]->team == GameTeam::ENEMY)
             mapFunc[objects[i]->type](world, server, i);
-        }
-    }
     return 0;
 }
