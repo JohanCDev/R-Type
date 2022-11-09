af698dbf4a1b80b1e0351d2a99e767f956b02a48
Johan Chrillesen <johan.chrillesen@epitech.eu>
2022-11-09 11:03:28 +0100

    [ADD] Doc of world

--- src/ECS/World.hpp
+++ src/ECS/World.hpp
@@ -61,46 +61,52 @@ class World {
     /**
      * @brief Create a laser entity
      *
-     * @param object
-     * @param team
-     * @param pos
-     * @param speed
-     * @param refresh_time
+     * @param object type of the object
+     * @param team Team of the laser
+     * @param pos base pos of the laser as a Vector2f. ex: {150.0, 120.0}
+     * @param speed base speed of the laser as a Vector2i. ex: {0, 0}
+     * @param refresh_time time before the laser is refreshed
+     *
+     * @returns id of the laser
      */
     size_t create_laser(GameObject object, GameTeam team, Vector2f pos, Vector2i speed, float refresh_time);
 
     /**
      * @brief Create a player entity
      *
-     * @param object
-     * @param pos
-     * @param speed
-     * @param refresh_time
+     * @param object type of the object
+     * @param pos base pos of the player as a Vector2f. ex: {150.0, 120.0}
+     * @param speed base speed of the player as a Vector2i. ex: {0, 0}
+     * @param refresh_time time before the laser is refreshed
+     *
+     * @returns id of the player
      */
     size_t create_player(GameObject object, Vector2f pos, Vector2i speed, float refresh_time);
 
     /**
      * @brief Create a enemy entity
      *
-     * @param object
-     * @param pos
-     * @param speed
-     * @param health
-     * @param refresh_time
+     * @param object type of the object
+     * @param pos base pos of the enemy as a Vector2f. ex: {150.0, 120.0}
+     * @param speed base speed of the enemy as a Vector2i. ex: {0, 0}
+     * @param health base health of the enemy as an unsigned int
+     * @param refresh_time time before the laser is refreshed
+     *
+     * @returns id of the enemy
      */
     size_t create_enemy(GameObject object, Vector2f pos, Vector2i speed, size_t health, float refresh_time);
 
     /**
      * @brief Create a skills entity
      *
-     * @param pos
+     * @param pos base pos of the skill object as a Vector2f. ex: {150.0, 120.0}
      */
     void create_skills(Vector2f pos);
 
     /**
      * @brief settings button
      *
-     * @param pos
+     * @param pos base pos of the settings button as a Vector2f. ex: {150.0, 120.0}
      */
     void create_settings(Vector2f pos);
 
@@ -108,24 +114,93 @@ class World {
      * @brief settings button
      *
      * @param life percentage of life remaining
-     * @param pos
      */
     void create_healthbar(float life);
 
+    /**
+     * @brief Registers all drawable objects
+     *
+     */
     void register_all_drawable_object();
 
+    /**
+     * @brief Get the Registry object
+     *
+     * @return Reference to the registry
+     */
     registry &getRegistry();
+
+    /**
+     * @brief Get the Resources Manager object
+     *
+     * @return Reference to the resources manager
+     */
     ResourcesManager &getResourcesManager();
+
+    /**
+     * @brief Get the Window object
+     *
+     * @return Reference to the renderWindow object
+     */
     sf::RenderWindow &getWindow();
+
+    /**
+     * @brief Get the Clock object
+     *
+     * @return Reference to the clock object
+     */
     sf::Clock &getClock();
+
+    /**
+     * @brief Get the Direction object
+     *
+     * @return Reference to the direction
+     */
     Vector2i &getDirection();
+
+    /**
+     * @brief Set the Direction object
+     *
+     * @param direction new direction as a Vector2i. ex: {0, 0}
+     */
     void setDirection(Vector2i direction);
 
   private:
+    /**
+     * @brief Contains the registry object
+     *
+     */
     registry _r;
+
+    /**
+     * @brief Contains the resources manager object
+     *
+     */
     ResourcesManager _manager;
+
+    /**
+     * @brief SFML window used to draw textures
+     *
+     */
     sf::RenderWindow _window;
+
+    /**
+     * @brief Clock of the game
+     *
+     */
     sf::Clock _clock;
+
+    /**
+     * @brief Map containing all the drawable components
+     *
+     * The key is the type of the GameObject, the value being the drawableComponent to display
+     *
+     */
     std::map<GameObject, DrawableComponent> _drawMap;
+
+    /**
+     * @brief Player actual direction
+     *
+     */
     Vector2i _player_direction;
 };
