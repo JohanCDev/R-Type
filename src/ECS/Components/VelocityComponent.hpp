efb1377a0a6ed87ecc665ec5e516990abc2915a3
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-11-09 09:45:35 +0100

    [FIX] Fit CLang

--- src/ECS/Components/VelocityComponent.hpp
+++ src/ECS/Components/VelocityComponent.hpp
@@ -15,7 +15,7 @@
 
 /**
  * @brief Give a velocity to the entity
- * 
+ *
  */
 struct VelocityComponent {
     Vector2i speed;
@@ -24,11 +24,11 @@ struct VelocityComponent {
 
     /**
      * @brief Construct a new Velocity Component object
-     * 
-     * @param x_velo 
-     * @param y_velo 
-     * @param time_as_seconds 
-     * @param elapsed_time 
+     *
+     * @param x_velo
+     * @param y_velo
+     * @param time_as_seconds
+     * @param elapsed_time
      */
     VelocityComponent(Vector2i speed_vec, float time_as_seconds, float elapsed_time = 0.0)
         : speed(speed_vec), seconds(elapsed_time), coeff(time_as_seconds)
