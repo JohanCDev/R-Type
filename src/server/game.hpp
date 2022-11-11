f6badc1353b0106622ce003cc3865eb9d9fa37d1
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-11-11 16:00:17 +0100

    [REF] Change default speed

--- src/server/game.hpp
+++ src/server/game.hpp
@@ -107,17 +107,17 @@ typedef struct myValues_s {
  * @brief Default values for each GameObject (pos, hp, atk, spd, scale)
  */
 static std::map<GameObject, myValues_t> defaultValues = {
-    {GameObject::LASER, {{-1, -1}, 1, -1, 5, 1.0}},
-    {GameObject::PLAYER, {{64, 540}, 100, 100, 8, 2.0}},
-    {GameObject::SHIP_ARMORED, {{64, 540}, 150, 50, 6, 1.0}},
-    {GameObject::SHIP_DAMAGE, {{64, 540}, 80, 120, 6, 1.0}},
-    {GameObject::SHIP_ENGINEER, {{64, 540}, 100, 100, 6, 1.0}},
-    {GameObject::SHIP_SNIPER, {{64, 540}, 150, 50, 6, 1.0}},
-    {GameObject::BOSS_1, {{1920, 300}, 100, 40, 6, 2.0}},
-    {GameObject::ENEMY_FOCUS, {{1920, -1}, 100, 40, 6, 1.0}},
-    {GameObject::ENEMY_KAMIKAZE, {{1920, -1}, 100, 40, 12, 1.0}},
-    {GameObject::ENEMY_ODD, {{1920, -1}, 100, 40, 6, 1.0}},
-    {GameObject::ENEMY_SNIPER, {{1920, -1}, 100, 40, 6, 1.0}},
+    {GameObject::LASER, {{-1, -1}, 1, -1, 14, 1.0}},
+    {GameObject::PLAYER, {{64, 540}, 100, 100, 12, 2.0}},
+    {GameObject::SHIP_ARMORED, {{64, 540}, 150, 50, 12, 1.0}},
+    {GameObject::SHIP_DAMAGE, {{64, 540}, 80, 120, 12, 1.0}},
+    {GameObject::SHIP_ENGINEER, {{64, 540}, 100, 100, 12, 1.0}},
+    {GameObject::SHIP_SNIPER, {{64, 540}, 150, 50, 12, 1.0}},
+    {GameObject::BOSS_1, {{1920, 300}, 100, 40, 8, 2.0}},
+    {GameObject::ENEMY_FOCUS, {{1920, -1}, 100, 40, 10, 1.0}},
+    {GameObject::ENEMY_KAMIKAZE, {{1920, -1}, 100, 40, 20, 1.0}},
+    {GameObject::ENEMY_ODD, {{1920, -1}, 100, 40, 10, 1.0}},
+    {GameObject::ENEMY_SNIPER, {{1920, -1}, 100, 40, 10, 1.0}},
 };
 
 /**
