041b638a711779b1babb5a60abe41ee4a4b78538
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-11-13 17:43:39 +0100

    [ADD] Break when found button

--- src/ECS/Systems/ClickableSystem.cpp
+++ src/ECS/Systems/ClickableSystem.cpp
@@ -37,10 +37,10 @@ int clickable_system(World &world, Vector2i click_pos, SceneScreen &current_scre
 
     for (size_t i = 0; i < clickables.size(); ++i) {
         auto &clickable = clickables[i];
-
         if (clickable && clickable.has_value()) {
             if (pos_intersects(click_pos, drawables[i].value(), positions[i].value()) == true) {
                 (clickable->callback)(world, current_screen, client);
+                break;
             }
         }
     }
